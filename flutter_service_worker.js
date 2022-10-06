e5dbf938b5ba68f072a03a6c0c0817ba1ca86463
Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
2022-10-06 20:03:33 -0300

    Deploy build from pipeline

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "ccaf192e8f4378ec93e8c7fb118fa5bb",
-"index.html": "a1c93a710395eb13967ef0e6b87f0e79",
-"/": "a1c93a710395eb13967ef0e6b87f0e79",
+"index.html": "66750df7dda9775ad0d36e140b318b00",
+"/": "66750df7dda9775ad0d36e140b318b00",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "9645fc713768c81365818adbff76bb3e",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
