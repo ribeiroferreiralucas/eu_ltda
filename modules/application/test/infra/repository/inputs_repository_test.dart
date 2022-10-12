2f729bec12d579ab1b2c10df26a5312bc71e1c36
Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
2022-10-11 21:17:46 -0300

    Add Repository more focused integration tests

--- modules/application/test/infra/repository/inputs_repository_test.dart
+++ modules/application/test/infra/repository/inputs_repository_test.dart
@@ -1,9 +1,12 @@
+import 'dart:ffi';
+
 import 'package:application/infra/drift_wrapper/drift_database.dart';
 import 'package:application/infra/repository/inputs_repository.dart';
+import 'package:drift/drift.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:model/input.dart' as domain;
 
 
-var newInput = InputsCompanion.insert;
 void main() {
   group('The Inputs Repository', () {
     late EuLtdaDriftDatabase database;
@@ -19,22 +22,39 @@ void main() {
     });
 
     group('WHEN Listing All data', () {
-      test("SHOULD list all data", () async {
-        await insertData(database, [newInput(name: 'anyName', buyQnt: 0.5, mesureUnit: 'units')]);
+      test("SHOULD retrieve registered Input as Input class with all registered values", () async {
+        var inputTableRegistry = newInputTableRegistry(id: 1);
+        await insertData(database, [inputTableRegistry]);
 
-        var list = await inputsRepository.listAll();
-        expect(list.length, equals(1));
+        List<domain.Input> list = await callListAll(inputsRepository);
+
+        var expectedInput = domain.Input(1, inputTableRegistry.name.value, inputTableRegistry.buyQnt.value, inputTableRegistry.mesureUnit.value);
+        expect(list[0], expectedInput);
+      });
+
+      test("SHOULD list all data", () async {
+        await insertData(database, [newInputTableRegistry(), newInputTableRegistry(), newInputTableRegistry(), newInputTableRegistry()]);
+
+        List<domain.Input> list = await callListAll(inputsRepository);
+        expect(list.length, equals(4));
       });
 
       test("SHOULD return empty list IF there is no data on database", () async {
-        var list = await inputsRepository.listAll();
+        List<domain.Input> list = await callListAll(inputsRepository);
         expect(list.length, equals(0));
       });
     });
   });
 }
 
+
+InputsCompanion newInputTableRegistry({int? id, String name = 'anyName', double buyQnt = 1000, String mesureUnit = 'units'}) => 
+    InputsCompanion.insert(id: Value.ofNullable(id), name: name, buyQnt: buyQnt, mesureUnit: mesureUnit);
+
+Future<List<domain.Input>> callListAll(InputsRepository inputsRepository) async {
+  var list = await inputsRepository.listAll();
+  return list;
+}
 Future<void> insertData(EuLtdaDriftDatabase database, List<InputsCompanion> datas) {
   return database.batch((batch) => batch.insertAll(database.inputs, datas));
 }
-
